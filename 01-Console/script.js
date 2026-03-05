d5b26c303bc5ee700ad4d38006e6c4fc1e7fb84f
VIRENDRA-18 <virusingh0125@gmail.com>
2026-03-05 11:59:18 +0530

    first commit

---
+++ 01-Console/script.js
@@ -0,0 +1,30 @@
+console.log(100);
+
+a = 10;
+b = 70;
+console.log(a + b);
+
+const x = 79;
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({Name : 'Virendra', Age : 23, Company : 'Celebal Tech'});
+
+console.group('sample');
+
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding : 10px; background-color : white; color : green';
+
+console.log('%cHello Viru', styles);
+
+
+
+
+
---
+++ 02-variable/script.js
@@ -0,0 +1,84 @@
+// way to declear the variable
+
+// var, let, const
+
+let firstName = 'Viru';
+let lastName = 'Singh';
+
+console.log(firstName, lastName);
+
+let Age = 23;
+
+console.log(Age);
+
+// Naming Conventions
+//  - Only Letters, Numbers, Underscore, and Doller sign
+// - Can't start with a Number
+
+// Muti-Word Formatting
+// firstName = camleCase
+// first_num = Underscore
+// FirstName = PascalCase
+// firstname = lowercase
+
+
+// Re-Assigning Variable 
+
+Age = 26;
+
+console.log(Age);
+
+
+let score;
+
+score = 5;
+
+console.log(score);
+
+if (true) {
+    score = score + 1;
+}
+
+console.log(score);
+
+
+const x = 100;
+
+
+console.log(x);
+
+const arr = [1,2,3,4];
+
+arr.push(5);
+
+console.log(arr);
+
+const person = {
+    Name : 'Viru Singh'
+}
+
+person.Name = 'Virendra';
+person.Email = 'viru@gmail.com';
+person.Age = 23;
+
+
+console.log(person);
+
+
+// Declear multipal values at once
+
+let a,b,c;
+
+a = 10;
+b = 20;
+c = 12;
+
+console.log(c);
+
+const d = 13,
+  e = 21,
+  f = 22;
+
+  console.log(d);
+
+
