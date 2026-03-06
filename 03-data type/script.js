c49a2ef3d6a80281421e986995d12819934f8d35
VIRENDRA-18 <virusingh0125@gmail.com>
2026-03-06 11:46:09 +0530

    stact-heap and type conversion

---
+++ 03-data type/script.js
@@ -0,0 +1,71 @@
+// String 
+
+const firstName = 'Kumkum';
+
+const output = firstName;
+
+console.log(output, typeof output);
+
+//Number 
+
+const age = 23;
+
+const num =  89.7;
+
+console.log(age, typeof age);
+console.log(num, typeof num);
+
+// Boolean
+
+let haskits = true;
+
+console.log(haskits, typeof haskits);
+
+// Null 
+
+const aptNumber = null;
+
+console.log(aptNumber, typeof aptNumber);
+
+// Undefined
+
+// let score ;
+
+// console.log(score, typeof score);
+
+const score = undefined;
+
+console.log(score, typeof score);
+
+// Symbol
+
+
+
+// Reference Type data
+
+
+// Array
+
+const number = [1,2,3,4];
+
+console.log(number, typeof number);
+
+// Object
+
+const studentDetailed = {
+    Name : 'Viru',
+    Age : 23,
+    Email : 'virusingh012@gmail.com',
+    Role : 'React Developer',
+    Company : 'Celebal Tech'
+}
+
+console.log(studentDetailed);
+
+// Function
+
+function sayHello () {
+    console.log('Hii Viru ! How are you');
+}
+
+console.log(sayHello(), typeof sayHello);
