2e40dbb8bdf41405fde6d8e88cf5512ea9cd1e8d
VIRENDRA-18 <virusingh0125@gmail.com>
2026-03-20 12:44:18 +0530

    learn more about javascript concept like Operator and type conversion

---
+++ 05-type-conversion/script.js
@@ -0,0 +1,31 @@
+ let amount = 'hello';
+
+ //change string to number 
+//  amount = parseInt(amount);
+//  amount =+ amount;
+//  amount = Number(amount);
+
+//  console.log(amount, typeof amount);
+
+ // change number to string
+
+//  amount = amount.toString();
+//  amount = String(amount);
+
+
+// change string to decimal number
+
+amount = parseFloat(amount);
+
+// change number to boolean
+
+// amount = Boolean(amount);
+
+
+console.log(Math.sqrt(-1));
+console.log(undefined + undefined);
+console.log(NaN + 9);
+console.log('viru' / 8);
+
+console.log(amount, typeof amount);
+
---
+++ 06-operators/script.js
@@ -0,0 +1,69 @@
+// Arithmatic operator
+
+let x ;
+
+x = 5 + 5;
+x = 10 - 3;
+x = 12 * 3;
+x = 10 / 5;
+x = 2 ** 2;
+x = 22 % 5;
+
+// Concantenation
+
+x = "Viru" + " " + "Singh";
+
+x = 'Virat' + ' Panday';
+
+// Exponet
+
+x = 2 ** 3;
+
+// Increment
+
+x = 1;
+
+x = x + 1;
+
+x++;
+
+++x;
+
+// Decrement 
+
+x = 5;
+
+x = x - 1;
+
+x--;
+
+--x;
+
+
+// Assignment Operator
+
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 2;
+x /= 5;
+
+
+// Comparsion operator
+
+x = 2 == 2;
+
+x = 2 === '2';
+
+x = 2 != 2;
+
+x = 2 !== '2';
+
+x = 10 < 9;
+x = 10 > 12;
+x = 10 <= 5;
+x = 10 >= 5;
+
+
+console.log(x);
